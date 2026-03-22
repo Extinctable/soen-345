940537c31ca2358ef09ec772fe7a464b6a633727
Extinctable <massimo02caruso@gmail.com>
2026-03-22 00:08:42 -0400

    Sonarcloud workflow
    Code coverage workflow with sonarcloud

--- TicketReservationApp/build.gradle.kts
+++ TicketReservationApp/build.gradle.kts
@@ -1,4 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    id("org.sonarqube") version "7.2.3.7755"
+}
+
+sonar {
+  properties {
+    property ("sonar.projectKey", "Extinctable_soen-345")
+    property ("sonar.organization", "extinctable")
+  }
 }
